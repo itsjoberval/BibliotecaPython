06421c31e7943857f8c1415a4e0af81e35d53f40
Joberval Magalhães dos Santos <29413697+itsjoberval@users.noreply.github.com>
2024-11-26 20:40:36 -0300

    configuração BD tests
    Configuração do banco de dados em memória para testes.

--- ResolucaodoExercicio/test_biblioteca.py
+++ ResolucaodoExercicio/test_biblioteca.py
@@ -1,3 +1,21 @@
 import sqlite3
 import pytest
 from main import Biblioteca
+
+@pytest.fixture
+def conexao_teste():
+    conexao = sqlite3.connect(":memory:")
+    cursor = conexao.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE Usuario (
+            id TEXT PRIMARY KEY,
+            nome TEXT NOT NULL,
+            nacionalidade TEXT NOT NULL,
+            telefone TEXT NOT NULL
+        )
+        """
+    )
+    conexao.commit()
+    yield conexao
+    conexao.close()
--- ResolucaodoExercicio/test_livro.py
+++ ResolucaodoExercicio/test_livro.py
@@ -1,3 +1,32 @@
 import sqlite3
 import pytest
 from main import Livro
+
+@pytest.fixture
+def conexao_teste():
+    conexao = sqlite3.connect(":memory:")
+    cursor = conexao.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE Livro (
+            titulo TEXT PRIMARY KEY,
+            autor TEXT NOT NULL,
+            editora TEXT NOT NULL,
+            genero TEXT NOT NULL,
+            qtd_exemplares INTEGER NOT NULL
+        )
+        """
+    )
+    cursor.execute(
+        """
+        CREATE TABLE emprestimos (
+            titulo TEXT NOT NULL,
+            id_usuario TEXT NOT NULL,
+            data_emprestimo TEXT NOT NULL,
+            data_devolucao TEXT
+        )
+        """
+    )
+    conexao.commit()
+    yield conexao
+    conexao.close()
